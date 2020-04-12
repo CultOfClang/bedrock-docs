b5e58f4bd613036fead31025060d5846aad4247b
lizelive <github@lize.live>
2020-04-12 00:02:52 -0700

    Delete download.sh

--- download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd $GITHUB_REF
-rm -rf behavior resource
-curl -L https://aka.ms/behaviorpacktemplate -o behaviorpacktemplate.zip
-unzip behaviorpacktemplate.zip -d behavior
-rm -f behaviorpacktemplate.zip
-curl -L https://aka.ms/behaviorpacktemplate -o resourcepacktemplate.zip
-unzip resourcepacktemplate.zip -d resource
-rm -f resourcepacktemplate.zip
