ce7b76d651258b1d86bb70fde7061bfe5acbbb3c
lizelive <github@lize.live>
2021-12-30 14:41:53 -0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 cd $GITHUB_WORKSPACE
-rm -rf behavior resource beta
+rm -rf behavior resource beta stable
+
+mkdir stable
+cd stable
 curl -L https://aka.ms/behaviorpacktemplate -o behaviorpacktemplate.zip
 unzip behaviorpacktemplate.zip -d behavior
 rm -f behaviorpacktemplate.zip
 curl -L https://aka.ms/resourcepacktemplate -o resourcepacktemplate.zip
 unzip resourcepacktemplate.zip -d resource
 rm -f resourcepacktemplate.zip
+cd -
 
 #download beta
 mkdir beta
@@ -18,3 +22,4 @@ rm -f behaviorpacktemplate.zip
 curl -L https://aka.ms/MinecraftBetaResources -o resourcepacktemplate.zip
 unzip resourcepacktemplate.zip -d resource
 rm -f resourcepacktemplate.zip
+cd -
